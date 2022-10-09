6a59374fc7275d402000f847b246cc3dcc543e09
@SAYO <91386506+sayo-api@users.noreply.github.com>
2022-10-09 08:22:46 -0300

    Update init.js

--- init.js
+++ init.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
 
+//app.use(express.static('arquivos'));
+
+app.get('/', async (req, res) => { 
+    res.sendFile('index.html', {root: __dirname })
+});
+
+
+const porta = process.env.PORT || 5000;
+//iniciando...
+app.listen(porta, () => console.log("site Online na porta:", porta));
